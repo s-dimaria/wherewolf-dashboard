f18e297aa3745833de6031743690c459153af57b
Luca Sacco <saccoluca9@gmail.com>
2026-06-17 14:07:56 +0200

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -580,10 +580,10 @@ export default function App() {
               <col style={{ width: '9%' }} />  
               <col style={{ width: '13%' }} /> 
               <col style={{ width: '13%' }} /> 
-              <col style={{ width: '10%' }} /> 
+              <col style={{ width: '11%' }} /> 
               <col style={{ width: '14%' }} /> 
               <col style={{ width: '9%' }} />  
-              <col style={{ width: '9%' }} /> 
+              <col style={{ width: '8%' }} /> 
             </colgroup>
             <thead>
               <tr>
